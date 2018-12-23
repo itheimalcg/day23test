acd52220385790fbf0ae9cc5f4f49d0f6f73eb87
lcg <1627182726@qq.com>
2018-12-23 20:00:17 +0800

    第一次提交文件

---
+++ src/com/itheima/git/firstGit.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @Package: com.itheima.git
+ * @author: lichangguo
+ * @Description：
+ * @date: 2018/12/22 19:41
+ */
+public class firstGit {
+    public static void main(String[] args) {
+        System.out.println("welcome boys!!");
+        System.out.println("welcome boys!!");
+        System.out.println("welcome boys!!");
+        System.out.println("welcome boys!!");
+        System.out.println("welcome boys!!");
+    }
+}
